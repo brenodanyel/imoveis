61c5fc2eb1b864a8567671f9786f1d23796ac6ca
iDz <bdls1704@hotmail.com>
2023-07-09 07:26:06 -0300

    udp

--- apps/www/pages/imoveis/index.vue
+++ apps/www/pages/imoveis/index.vue
@@ -1,6 +1,15 @@
 <template>
-	<div>
-		<h1>Imóveis</h1>
-		<p>Lista de imóveis</p>
+	<div class="flex h-full">
+		<div class="w-1/4 border-r">
+			<p>Filtros</p>
+		</div>
+
+		<div class="w-full px-5">
+			<p class="text-gray-800 text-3xl font-bold">Imóveis:</p>
+
+			<div class="row justify-between gap-3"></div>
+		</div>
 	</div>
 </template>
+
+<script setup lang="ts"></script>
